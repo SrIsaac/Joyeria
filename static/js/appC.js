85b52bb7bacf2eaff64195c59aabeb0b1dba5b6b
SrIsaac <isaac201275@gmail.com>
2023-10-07 15:51:41 -0500

    incio JS

---
+++ static/js/appC.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const email = {
+        nombre: "",
+        numero: "",
+        correo:"",
+        mensaje: "",
+    };
+
+    const inputNombre=document.querySelector("#nombre");
+    const inputNumero=document.querySelector("#numero");
+    const inputCorreo=document.querySelector("#correo");
+    const inputMensaje=document.querySelector("#mensaje");
+    const ButtonEnviar=document.querySelector("enviar");
+
+    
+
+});
